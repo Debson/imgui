a54f0f8cd32a93abb09dc31f06976385685ee975
debsonosbed <debsonosbed@gmail.com>
2020-09-25 12:42:16 +0100

    - Updated premake5 config file

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
-	language "C++"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -21,14 +23,9 @@ project "ImGui"
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "system:linux"
-		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "configurations:Debug"
 		runtime "Debug"
